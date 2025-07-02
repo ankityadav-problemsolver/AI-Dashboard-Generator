62cd2edcc3ef9182f87ee49a9b6e92969dc881ff
ankityadav-problemsolver <ankitofficial151@gmail.com>
2025-07-02 19:15:26 +0530

    fix the all bugs

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score
-from fpdf import FPDF
+# from fpdf import FPDF
 import base64
 from io import BytesIO
 import os
